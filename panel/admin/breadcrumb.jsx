918ffcee9ede58338ed03875f307fa4ea310ad25
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 15:00:59 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='assignmentReturn'
+    name='returns'
+    title='returnsReturnRequests'
+    {...props}
+/>
